da6d6f13e50763e5ccff94c2d405d790afdea7f9
Vihaan Sharma <sharmavihaan190@gmail.com>
2023-12-12 18:47:48 -0500

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,51 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "../components/Navbar";
-import { AuthContextProvider } from "./context/AuthContext";
-import AppContainer from "./AppContainer";
-import Dialog from "@/components/Dialog";
-import BottomNavbar from "@/components/BottomNavbar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '../components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
+import AppContainer from './AppContainer';
+import Dialog from '@/components/Dialog';
+import BottomNavbar from '@/components/BottomNavbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://d87b-142-115-122-93.ngrok-free.app/"),
-  title: "WatchWave",
-  description: "WatchWave is a free streaming service for movies and TV shows.",
-  keywords:
-    "watch movies, movies online, watch TV, TV online, TV shows online, watch TV shows, stream movies, stream tv, instant streaming, watch online, movies, watch movies United States, watch TV online, no download, full length movies watch online, movies online, movies, watch movies online, watch movies, watch movies online free, watch movies for free, watch streaming media, watch tv online, watch movies online, watch movies online free, watch movies for free, watch streaming media, watch tv online",
-  icons: {
-    icon: "/favicon.svg",
-    apple: "/favicon.svg",
-  },
-  openGraph: {
-    type: "website",
-    url: `https://d87b-142-115-122-93.ngrok-free.app`,
-    images: [
-      {
-        url: "/Meta.png",
-        alt: "WatchWave Logo",
-      },
-    ],
-  },
-  // add image
+	metadataBase: new URL('https://d87b-142-115-122-93.ngrok-free.app/'),
+	title: 'WatchWave',
+	description: 'WatchWave is a free streaming service for movies and TV shows.',
+	keywords:
+		'watch movies, movies online, watch TV, TV online, TV shows online, watch TV shows, stream movies, stream tv, instant streaming, watch online, movies, watch movies United States, watch TV online, no download, full length movies watch online, movies online, movies, watch movies online, watch movies, watch movies online free, watch movies for free, watch streaming media, watch tv online, watch movies online, watch movies online free, watch movies for free, watch streaming media, watch tv online',
+	icons: {
+		icon: '/favicon.svg',
+		apple: '/favicon.svg',
+	},
+	openGraph: {
+		type: 'website',
+		url: `https://d87b-142-115-122-93.ngrok-free.app`,
+		images: [
+			{
+				url: '/Meta.png',
+				alt: 'WatchWave Logo',
+			},
+		],
+	},
+	// add image
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      </head>
-      <body className={inter.className + " bg-background text-foreground dark"}>
-        <AppContainer>
-          <Navbar />
-          <BottomNavbar />
-          {children}
-        </AppContainer>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			</head>
+			<body className={inter.className + ' bg-background text-foreground dark'}>
+				<AppContainer>
+					<Navbar />
+					<BottomNavbar />
+					{children} <Analytics />
+				</AppContainer>
+			</body>
+		</html>
+	);
 }
